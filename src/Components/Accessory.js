9d03fea9c76a3b393a32e024b4b78784259988f8
Sammy Mureithi <sammymureithi20@gmail.com>
2022-09-27 21:09:00 +0300

    Pop

--- src/Components/Accessory.js
+++ src/Components/Accessory.js
@@ -5,13 +5,125 @@ import { Typography } from '@mui/material'
 import React, { useState } from 'react'
 import AlertDialog from './PopOverDashBoard'
 
-function Accessory( { title, amount } ) {
+function Accessory( { title, amount ,classname} ) {
   const [open, setOpen] = useState( false );
   function handleClick() {
     setOpen( true );
   }
   function handleClose() {
     setOpen( false );
+  }
+ const pop= function showPopupDialog() {
+    if ( classname === "Others" ) {
+      return (
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Your Current Stock"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'></Typography>
+              <Typography variant='h8'>Refills</Typography>
+              <Typography variant='h8'>Empty</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>3Kg</Typography>
+              <Typography variant='h8'>0</Typography>
+              <Typography variant='h8'>10</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>22.5Kg</Typography>
+              <Typography variant='h8'>0</Typography>
+              <Typography variant='h8'>10</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>45Kg</Typography>
+              <Typography variant='h8'>0</Typography>
+              <Typography variant='h8'>10</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>50Kg</Typography>
+              <Typography variant='h8'>0</Typography>
+              <Typography variant='h8'>10</Typography>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+        </Dialog>
+      )
+    }
+    else if ( classname === "Acessory" ) {
+      return (
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Your Accessory Stock"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>Burner:</Typography>
+              <Typography variant='h8'>0</Typography>
+              </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>Grill</Typography>
+              <Typography variant='h8'>0</Typography>
+              </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>Regulator</Typography>
+              <Typography variant='h8'>0</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>HosePipe</Typography>
+              <Typography variant='h8'>0</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>Oil</Typography>
+              <Typography variant='h8'>0</Typography>
+            </DialogContentText>
+            <DialogContentText id="alert-dialog-description1">
+            <Typography variant='h8'>Others</Typography>
+              <Typography variant='h8'>0</Typography>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+        </DialogActions>
+        </Dialog>
+      )
+   }
+    else {
+      return (
+        <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Use Google's location service?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending anonymous
+              location data to Google, even when no apps are running.{classname}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+          <Button onClick={handleClose}>Allaa</Button>
+        </DialogActions>
+        </Dialog>
+      )
+    }
   }
   return (
     <>
@@ -20,25 +132,7 @@ function Accessory( { title, amount } ) {
           <Typography variant='h5'>{title}</Typography>
           <Typography variant='h9'>{amount}</Typography>
       </Box>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Use Google's location service?"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-        </DialogActions>
-      </Dialog>
+      {pop()}
     </>
     
   )
--- src/Pages/DashBoard.js
+++ src/Pages/DashBoard.js
@@ -49,10 +49,10 @@ function DashBoard() {
                                 empyty={data.ttl13kgCylinder}/>
                         </div>
                         <div className='others'>
-                            <Accessory title="Others" amount="3kg,22.5kg,45kg,50kg" />
-                            <Accessory title={"Accessory"} amount="0" />
-                            <Accessory title={"Todays Sales"} amount="Kes.20,192" />
-                            <Accessory title={"Todays Expenses"} amount="Kes.192" />
+                            <Accessory title="Others" amount="3kg,22.5kg,45kg,50kg"  classname="Others"/>
+                            <Accessory title={"Accessory"} amount="0"  classname="Acessory"/>
+                            <Accessory title={"Todays Sales"} amount="Kes.20,192"  classname="TodaysSales"/>
+                            <Accessory title={"Todays Expenses"} amount="Kes.192"  classname="TodaysExpenses"/>
                         </div>
                         <Fab color="primary" aria-label="add" id='fab'>
                             <AddIcon />
