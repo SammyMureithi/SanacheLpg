344c348be264c263a017eb0558f216a3e967cab1
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-07 02:40:17 +0300

    Imekuwa moto

--- src/Pages/CustomerAccount.js
+++ src/Pages/CustomerAccount.js
@@ -1,17 +1,22 @@
+
+import axios from 'axios';
+import { saveAs } from 'file-saver';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 
 function CustomerAccount() {
-    const { CustomerId } = useParams();
-    const [data, setData] = useState( "Mure" );
+    const {CustomerName, CustomerId } = useParams();
+    const [data, setData] = useState("");
     const customer_id = new FormData();
     customer_id.append( "customer_id", CustomerId );
+    
  
     useEffect( () => {
+     
         fetch( "https://sabugostores.co.ke/F1jw1PiAZwU-sanacheLPG/php/reports/customer_account.php", {
             method: "POST",
             headers: {
-               
+               'Content-Type':"application/pdf"
             },
             body: customer_id
         } )
@@ -28,6 +33,7 @@ function CustomerAccount() {
     console.log( data );
   return (
       <div>
+          
           <>Imekuwa motoo </>
     </div>
   )
