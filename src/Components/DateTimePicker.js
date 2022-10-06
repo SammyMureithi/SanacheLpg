f4c10da3f3210ffaa078254c6aa37d747bbc910a
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-06 22:24:16 +0300

    DateTimePicker Component

--- src/Components/DateTimePicker.js
+++ src/Components/DateTimePicker.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react'
-
-import { DesktopDateTimePicker } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DesktopDateTimePicker, LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { TextField } from '@mui/material';
 
 function DateTimePickerComponent() {
-    const [currentDate, setCurrentData] = useState(new Date());
+  const [currentDate, setCurrentData] = useState( new Date() );
+  function handleChange( e ) {
+    setCurrentData( e.target.value );
+  }
   return (
-      <div>
-            <DesktopDateTimePicker
+    <div>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DesktopDateTimePicker
             label="Date desktop"
             inputFormat="MM/DD/YYYY"
             value={currentDate}
-           // onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-        />
+            onChange={handleChange}
+            renderInput={(params) => <TextField {...params} />}/>
+      </LocalizationProvider> 
     </div>
   )
 }
