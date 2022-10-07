af453d1b06b034389e0b49e6b96f637dc391b446
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-07 22:32:51 +0300

    Add Expenses UI successful

--- src/Pages/ExpensesPage.js
+++ src/Pages/ExpensesPage.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Button, CardContent, TextField } from '@material-ui/core'
+import { Card } from '@mui/material'
+import React, { useState } from 'react'
 
 function ExpensesPage() {
+  const [expense, setExpense] = useState( { Amount: "", description: "" } );
+  function handleChange( e ) {
+    setExpense( prevState => {
+      return {...prevState,[e.target.name]:e.target.value}
+    })
+  }
   return (
-    <div>ExpensesPage</div>
+    <div style={{display:"flex",flexDirection:"column"}}>
+      <div className='expensesBackBtn'>
+        <Button variant='contained' color="primary">Back</Button>
+      </div>
+      <Card className='formExpenseCard'>
+        <CardContent className='formExpense'>
+          <TextField label="Amount" variant="outlined" name='Amount'
+            type={"number"} value={expense.Amount} onChange={ handleChange} /><br></br>
+          <TextField label="Description" variant="outlined" onChange={handleChange}
+            name="description" type={"text"} value={expense.description} /><br></br>
+          <Button variant='contained' color='primary'>Add Expense</Button>
+        </CardContent>
+      </Card>
+    </div>
+    
   )
 }
 
