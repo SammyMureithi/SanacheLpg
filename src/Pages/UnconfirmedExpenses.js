9120174231374631eab1903f32bddc7ed67aea49
Sammy Mureithi <sammymureithi20@gmail.com>
2022-10-08 22:24:33 +0300

    Expenses Ui successful

--- src/Pages/UnconfirmedExpenses.js
+++ src/Pages/UnconfirmedExpenses.js
@@ -1,7 +1,8 @@
+import { Button, Card, CardContent, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react'
 
 function UnconfirmedExpenses() {
-    const [unconfirmedExpenses, setUnconfirmedExpenses] = useState( [] );
+    const [unconfirmedExpenses, setUnconfirmedExpenses] = useState( null );
     useEffect( () => {
         fetch( "https://sabugostores.co.ke/F1jw1PiAZwU-sanacheLPG/php/unconfirmedExpenses.php" )
             .then( res => {
@@ -16,9 +17,35 @@ function UnconfirmedExpenses() {
             } )
             .catch( error => console.log( error ) )
         
-    },[])
+    }, [] )
+    const res =  unconfirmedExpenses === null ? null : (
+        unconfirmedExpenses.map( element => {
+            return (
+                <Card key={element.expense_id} className="expenseCardItem">
+                     <CardContent >
+                    <Typography variant='h6'>Description: {element.description}</Typography>
+                    <Typography variant='h6'>Amount:{element.amount}</Typography>
+                    <Typography variant='h6'>Processed By:{element.processed_by}</Typography>
+                    <Typography variant='h6'>{element.processed_date}</Typography>
+                        <Typography variant='h6'>Shop:{element.shops_mobile_name}</Typography>
+                        <div className='expenseBtn'>
+                        <Button variant='contained' color='success'>Accept</Button>
+                        <Button variant='contained' color='error'>Reject</Button>
+                        </div>
+                        
+                </CardContent>
+                </Card>
+               
+            )
+        })
+    )
   return (
       <>
+          <Typography variant='h5'>Unconfirmed Expenses</Typography>
+          <Card className='masterExpenseCard'>
+          {res}
+          </Card>
+         
       </>
   )
 }
